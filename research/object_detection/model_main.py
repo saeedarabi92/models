092ffbb584e5ff728ae86a7e34827b3998ea494d
saeed <you@example.com>
2018-12-24 11:47:02 -0600

    update model_main.py and model_lib.py

--- research/object_detection/model_main.py
+++ research/object_detection/model_main.py
@@ -25,6 +25,7 @@ import tensorflow as tf
 from object_detection import model_hparams
 from object_detection import model_lib
 
+tf.logging.set_verbosity(tf.logging.INFO)
 flags.DEFINE_string(
     'model_dir', None, 'Path to output model directory '
     'where event and checkpoint files will be written.')
@@ -59,7 +60,7 @@ FLAGS = flags.FLAGS
 def main(unused_argv):
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir)
+  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, log_step_count_steps=1)
 
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,
